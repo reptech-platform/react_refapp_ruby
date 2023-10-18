fd9db1dc133d62f1f7a1facc3c99f1d3d1aa5cd0
sainikhila <sainikhila1999@gmail.com>
2023-10-18 17:07:00 +0530

    Updated with new api url http://3.133.64.67:8138/ecom/

--- src/shared/services.js
+++ src/shared/services.js
@@ -1,7 +1,10 @@
 
+//const serverApi = "http:/34.238.241.129:8082/";
+const serverApi = "http://3.133.64.67:8138/ecom/";
+
 const GetProductTypesApi = async (query) => {
-    let url = `http://34.238.241.129:8082/generated_app/ProductTypes`;
-    if (query) url = `http://34.238.241.129:8082/generated_app/ProductTypes?${query}`;
+    let url = `${serverApi}generated_app/ProductTypes`;
+    if (query) url = `${serverApi}generated_app/ProductTypes?${query}`;
 
     try {
         const res = await fetch(url, {
@@ -21,8 +24,8 @@ const GetProductTypesApi = async (query) => {
 }
 
 const GetProductTypesCount = async (query) => {
-    let url = `http://34.238.241.129:8082/generated_app/ProductTypes/$count`;
-    if (query) url = `http://34.238.241.129:8082/generated_app/ProductTypes/$count?${query}`;
+    let url = `${serverApi}generated_app/ProductTypes/$count`;
+    if (query) url = `${serverApi}generated_app/ProductTypes/$count?${query}`;
 
     try {
         const res = await fetch(url, {
@@ -43,8 +46,8 @@ const GetProductTypesCount = async (query) => {
 
 const GetProductsCount = async (query) => {
 
-    let url = `http://34.238.241.129:8082/generated_app/Products/$count`;
-    if (query) url = `http://34.238.241.129:8082/generated_app/Products/$count?${query}`;
+    let url = `${serverApi}generated_app/Products/$count`;
+    if (query) url = `${serverApi}generated_app/Products/$count?${query}`;
 
     try {
         const res = await fetch(url, {
@@ -65,8 +68,8 @@ const GetProductsCount = async (query) => {
 
 const GetProducts = async (query) => {
 
-    let url = `http://34.238.241.129:8082/generated_app/Products`;
-    if (query) url = `http://34.238.241.129:8082/generated_app/Products?${query}`;
+    let url = `${serverApi}generated_app/Products`;
+    if (query) url = `${serverApi}generated_app/Products?${query}`;
     //console.log(url);
 
     try {
@@ -88,7 +91,7 @@ const GetProducts = async (query) => {
 
 const SetProducts = async (data) => {
 
-    let url = `http://34.238.241.129:8082/generated_app/Products`;
+    let url = `${serverApi}generated_app/Products`;
 
     try {
         const res = await fetch(url, {
@@ -114,7 +117,7 @@ const SetProducts = async (data) => {
 
 const PatchProducts = async (id, data) => {
 
-    let url = `http://34.238.241.129:8082/generated_app/Products(${id})`;
+    let url = `${serverApi}generated_app/Products(${id})`;
 
     try {
         const res = await fetch(url, {
@@ -138,7 +141,7 @@ const PatchProducts = async (id, data) => {
 
 const GetProduct = async (id) => {
 
-    let url = `http://34.238.241.129:8082/generated_app/Products(${id})?$expand=ProductImage`;
+    let url = `${serverApi}generated_app/Products(${id})?$expand=ProductImage`;
 
     try {
         const res = await fetch(url, {
@@ -159,7 +162,7 @@ const GetProduct = async (id) => {
 
 const DeleteProduct = async (id) => {
 
-    let url = `http://34.238.241.129:8082/generated_app/Products(${id})`;
+    let url = `${serverApi}generated_app/Products(${id})`;
 
     try {
         const res = await fetch(url, {
@@ -182,7 +185,7 @@ const DeleteProduct = async (id) => {
 
 const GetProductImage = async (id) => {
 
-    let url = `http://34.238.241.129:8082/generated_app/Documents(${id})/$value`;
+    let url = `${serverApi}generated_app/Documents(${id})/$value`;
     // console.log(url);
 
     try {
@@ -204,7 +207,7 @@ const GetProductImage = async (id) => {
 
 const DeleteProductImage = async (id) => {
 
-    let url = `http://34.238.241.129:8082/generated_app/Documents(${id})/$value`;
+    let url = `${serverApi}generated_app/Documents(${id})/$value`;
 
     try {
         const res = await fetch(url, {
@@ -226,7 +229,7 @@ const DeleteProductImage = async (id) => {
 
 const SetProductImage = async (body, headers) => {
 
-    let url = `http://34.238.241.129:8082/generated_app/Documents`;
+    let url = `${serverApi}generated_app/Documents`;
 
     try {
         const res = await fetch(url, {
@@ -251,8 +254,8 @@ const SetProductImage = async (body, headers) => {
 }
 
 const SetProductTypesApi = async (httpMethod, description, productType) => {
-    let url = `http://34.238.241.129:8082/generated_app/ProductTypes`;
-    if (productType) url = `http://34.238.241.129:8082/generated_app/ProductTypes(${productType})`;
+    let url = `${serverApi}generated_app/ProductTypes`;
+    if (productType) url = `${serverApi}generated_app/ProductTypes(${productType})`;
     try {
         const res = await fetch(url, {
             method: httpMethod,
