eca3254cdb05bcf0ff7b83c3ed6ba06ad1268e71
sainikhila <sainikhila1999@gmail.com>
2024-02-20 11:50:45 +0530

    Update Drawer Menu items

--- src/components/drawer/index.js
+++ src/components/drawer/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Toolbar, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Tooltip } from '@mui/material';
 import MuiDrawer from '@mui/material/Drawer';
 import { styled, useTheme } from '@mui/material/styles';
-import { Pages, ShoppingBasket, Toc, EditNote, Checklist, Tab } from '@mui/icons-material';
+import { ShoppingBasket, Toc, EditNote, Checklist, Tab, GridView as GridViewIcon } from '@mui/icons-material';
 import { DRAWER_WIDTH } from "config";
 import { useNavigate } from 'react-router-dom';
 
@@ -103,6 +103,14 @@ const Component = (props) => {
                                 </Tooltip>
                             </ListItemIcon>
                             {open && <ListItemText primary="Tabbed Layout" sx={{ pl: 1 }} />}
+                        </ListItemButton>
+                        <ListItemButton onClick={() => NavigateTo("/producttiles")} sx={{ height: 50 }}>
+                            <ListItemIcon sx={{ minWidth: 30 }}>
+                                <Tooltip title="Tiles Layout">
+                                    <GridViewIcon />
+                                </Tooltip>
+                            </ListItemIcon>
+                            {open && <ListItemText primary="Tiles Layout" sx={{ pl: 1 }} />}
                         </ListItemButton>
                     </List>
                 </ListItem>
